845561cf0c5ace41e6af4f662e071de5b7082bde
mjh39088 <gjalswo3908@gmail.com>
2022-05-14 16:46:36 +0900

    .

--- app/src/main/java/com/example/mydiary/MainActivity.java
+++ app/src/main/java/com/example/mydiary/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mydiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 액티비티 (화면)이 실행될 때 가장 먼저 호출되는 곳
+        FloatingActionButton floatingActionButton = findViewById(R.id.btn_write);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 작성하기 버튼을 누를 때 호출되는 곳
+
+            }
+        });
     }
 }
