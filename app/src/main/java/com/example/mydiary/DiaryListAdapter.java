cb21c8e34bc59c3ae865a65a424afee73ed09ec4
mjh39088 <gjalswo3908@gmail.com>
2022-05-19 16:33:45 +0900

    .

--- app/src/main/java/com/example/mydiary/DiaryListAdapter.java
+++ app/src/main/java/com/example/mydiary/DiaryListAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,7 +14,7 @@ import java.util.ArrayList;
 
 //Alt + Enter 사용하면 좋음
 public class DiaryListAdapter extends RecyclerView.Adapter<DiaryListAdapter.ViewHolder> {
-    ArrayList<DiaryModel> mLst;
+    ArrayList<DiaryModel> mLstDiary;        // 다이어리 데이터들을 들고 있는 자료형 배열
     Context mContext;
 
     @NonNull
@@ -21,27 +23,70 @@ public class DiaryListAdapter extends RecyclerView.Adapter<DiaryListAdapter.View
         // 아이템 뷰가 최초로 생성이 될 때 호출되는 곳 = 생성주기 시작이 되는 곳
 
         mContext = parent.getContext();
-        View holder = LayoutInflater.from(mContext).inflate(R.layout.)
-        return null;
+        View holder = LayoutInflater.from(mContext).inflate(R.layout.list_item_diary, parent, false);
+        return new ViewHolder(holder);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DiaryListAdapter.ViewHolder holder, int position) {
         // 생성된 아이템 뷰가 실제 연동이 되어지는 곳
 
+        // 날씨의 경우의 수 작성
+        int weatherType = mLstDiary.get(position).getWeatherType();
+        switch (weatherType) {
+            case 0:
+                // 맑음
+                holder.iv_weather.setImageResource(R.drawable.img_sun);
+                break;
+            case 1:
+                // 흐림뒤갬
+                holder.iv_weather.setImageResource(R.drawable.img_cloudy);
+                break;
+            case 2:
+                // 흐림
+                holder.iv_weather.setImageResource(R.drawable.img_cloud);
+                break;
+            case 3:
+                // 매우흐림
+                holder.iv_weather.setImageResource(R.drawable.img_bad_cloud);
+                break;
+            case 4:
+                // 비
+                holder.iv_weather.setImageResource(R.drawable.img_rainy);
+                break;
+            case 5:
+                // 눈
+                holder.iv_weather.setImageResource(R.drawable.img_snowy);
+                break;
+        }
+        // 날씨 제목, 일시
+        String title = mLstDiary.get(position).getTitle();
+        String userDate = mLstDiary.get(position).getUserDate();
+
+        holder.tv_title.setText(title);
+        holder.tv_user_date.setText(userDate);
     }
 
     @Override
     public int getItemCount() {
         // 아이템 뷰의 총 갯수를 관리하는 곳
-        return 0;
+        return mLstDiary.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_weather;
+        TextView tv_title, tv_user_date;
+
         // 하나의 아이템 뷰
         public ViewHolder(@NonNull View itemView) {
-
             super(itemView);
+            iv_weather = itemView.findViewById(R.id.iv_weather);        // 날씨 이미지
+            tv_title = itemView.findViewById(R.id.tv_title);            // 다이어리 제목
+            tv_user_date = itemView.findViewById(R.id.tv_user_date);    // 사용자 지정 날짜
         }
+    }
+
+    public void setSampleList(ArrayList<DiaryModel> lstDiary) {
+        mLstDiary = lstDiary;
     }
 }
--- app/src/main/java/com/example/mydiary/MainActivity.java
+++ app/src/main/java/com/example/mydiary/MainActivity.java
@@ -8,9 +8,13 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRvDiary;              // 리사이클러 뷰 (리스트 뷰)
+    DiaryListAdapter mAdapter;          // 리사이클러 뷰와 연동할 어댑터
+    ArrayList<DiaryModel> mLstDiary;   // 리스트에 표현할 다이어리 데이터들 (배열)
 
     // var는 전역변수 사용 가능 val은 한 곳에서만 사용 가능 (메소드 안에서만)
     @Override
@@ -18,8 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLstDiary = new ArrayList<>();
+
         //fb 입력하면 findViewByID가 바로 나온다
         mRvDiary = findViewById(R.id.rv_diary);
+
+        mAdapter = new DiaryListAdapter(); // 리사이클러 뷰 어댑터 인스턴스 생성
+
+        // 다이어리 샘플 아이템 1개 생성
+        DiaryModel item = new DiaryModel();
+        item.setId(0);
+        item.setTitle("나는 오늘도 피곤하다");
+        item.setContent("허민재 내용입니다.");
+        item.setUserDate("2022/05/19 수");
+        item.setWriteDate("2022/05/19 수");
+        item.setWeatherType(0);
+        mLstDiary.add(item);
+
+        DiaryModel item2 = new DiaryModel();
+        item2.setId(0);
+        item2.setTitle("나는 오늘도 피곤하다");
+        item2.setContent("허민재 내용입니다.");
+        item2.setUserDate("2022/05/20 목");
+        item2.setWriteDate("2022/05/20 목");
+        item2.setWeatherType(3);
+        mLstDiary.add(item2);
+
+        mAdapter.setSampleList(mLstDiary);
+        mRvDiary.setAdapter(mAdapter);
 
         // 액티비티 (화면)이 실행될 때 가장 먼저 호출되는 곳
         FloatingActionButton floatingActionButton = findViewById(R.id.btn_write);
